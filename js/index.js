9c7ab012db67b90c9f6bfa345562426208ab7f80
kerimov528 <kerimov.anar528@gmail.com>
2023-03-05 23:32:18 -0800

    Fixed

--- js/index.js
+++ js/index.js
@@ -5,80 +5,80 @@ var subject = document.querySelector("#subjectInput")
 var textEra = document.querySelector("#validationTextarea");
 
 
-firstNa.addEventListener("focusout", ()=> {
-   validation(1);
+firstNa.addEventListener("focusout", () => {
+    validation(1);
 })
-lastNa.addEventListener("focusout", ()=> {
+lastNa.addEventListener("focusout", () => {
     validation(2);
- })
- email.addEventListener("focusout", ()=> {
+})
+email.addEventListener("focusout", () => {
     validation(3);
- })
- subject.addEventListener("focusout", ()=> {
+})
+subject.addEventListener("focusout", () => {
     validation(4);
- })
- textEra.addEventListener("focusout", ()=> {
+})
+textEra.addEventListener("focusout", () => {
     validation(5);
- })
+})
 
 
- validation = (number)=> {
-    switch(number) {
-        case 1: 
-        if(firstNa.value == "") {
-            firstNa.classList.remove("is-valid");
-            firstNa.classList.add("is-invalid")
-        }
-        else {
-            firstNa.classList.remove("is-invalid");
-            firstNa.classList.add("is-valid");
-    
-        };break;
-        case 2: 
-        if(lastNa.value == "") {
-            lastNa.classList.remove("is-valid");
-            lastNa.classList.add("is-invalid")
-        }
-        else {
-            lastNa.classList.remove("is-invalid");
-            lastNa.classList.add("is-valid");
-    
-        };break;
-        case 3:
-        if(email.value == "") {
-            email.classList.remove("is-valid");
-            email.classList.add("is-invalid")
-        }
-        else {
-            if(email.value.includes("@")){
-                email.classList.remove("is-invalid");
-                email.classList.add("is-valid");
+validation = (number) => {
+    switch (number) {
+        case 1:
+            if (firstNa.value == "") {
+                firstNa.classList.remove("is-valid");
+                firstNa.classList.add("is-invalid")
             }
-        else {
-            email.classList.add("is-invalid");
-            email.classList.remove("is-valid");
-        }
-    
-        };break;
-        case 4: 
-        if(subject.value == "") {
-            subject.classList.remove("is-valid");
-            subject.classList.add("is-invalid")
-        }
-        else {
-            subject.classList.remove("is-invalid");
-            subject.classList.add("is-valid");
-        };break;
-        case 5: 
-        if(textEra.value == "") {
-            textEra.classList.remove("is-valid");
-            textEra.classList.add("is-invalid")
-        }
-        else {
-            textEra.classList.remove("is-invalid");
-            textEra.classList.add("is-valid");
-        };break;
-        default:break;
+            else {
+                firstNa.classList.remove("is-invalid");
+                firstNa.classList.add("is-valid");
+
+            }; break;
+        case 2:
+            if (lastNa.value == "") {
+                lastNa.classList.remove("is-valid");
+                lastNa.classList.add("is-invalid")
+            }
+            else {
+                lastNa.classList.remove("is-invalid");
+                lastNa.classList.add("is-valid");
+
+            }; break;
+        case 3:
+            if (email.value == "") {
+                email.classList.remove("is-valid");
+                email.classList.add("is-invalid")
+            }
+            else {
+                if (email.value.includes("@")) {
+                    email.classList.remove("is-invalid");
+                    email.classList.add("is-valid");
+                }
+                else {
+                    email.classList.add("is-invalid");
+                    email.classList.remove("is-valid");
+                }
+
+            }; break;
+        case 4:
+            if (subject.value == "") {
+                subject.classList.remove("is-valid");
+                subject.classList.add("is-invalid")
+            }
+            else {
+                subject.classList.remove("is-invalid");
+                subject.classList.add("is-valid");
+            }; break;
+        case 5:
+            if (textEra.value == "") {
+                textEra.classList.remove("is-valid");
+                textEra.classList.add("is-invalid")
+            }
+            else {
+                textEra.classList.remove("is-invalid");
+                textEra.classList.add("is-valid");
+            }; break;
+        default: break;
     }
 }
 
